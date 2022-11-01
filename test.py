fc30c1e3cc94ea21f22fd202c75d1fda39f3a0f2
Ivan <dualking1991@gmail.com>
2022-11-01 18:08:37 +0400

    01.11.22

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
+import time
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+
+from path_to_website import path_to_website
+from path_to_webdriver import path_to_webdriver
+
 
 # Указываем путь к веб-драйверу
 driver = webdriver.Chrome(
-    executable_path='./chromedriver.exe'
+    executable_path=path_to_webdriver
 )
-# Открываем браузер на данной странице
-# Это специальный сайт для практики автотестирования
-driver.get('http://tutorialsninja.com/demo/')
 
 
+try:
+    # Открываем в браузере тестируемый сайт
+    driver.get(path_to_website)
+    # Ждем 5 секунд
+    time.sleep(5)
+except TimeoutException:
+    print('Timeout exception!')
+finally:
+    driver.close()
+    driver.quit()
+
+# Ищем на сайте различные элементы
+# name = 'search'
+# elem = driver.find_element(by=By.NAME, value=name)
+
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
