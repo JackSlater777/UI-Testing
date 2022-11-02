1391aa3fc5af36ae10fbef008f5de09edbb5ec18
Ivan <dualking1991@gmail.com>
2022-11-02 15:17:27 +0400

    02.11.22

--- patterns/selenium_beautifulsoup.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-
-with open("blank/index.html") as file:
-    # Помещаем код html-страницы в переменную
-    src = file.read()
-
-
-soup = BeautifulSoup(src, 'lxml')  # второй параметр - парсер
